19978fb67e68d91d603f7821e74502ec9c49a994
Alena Zueva <aleo.zueva@gmail.com>
2020-02-09 21:25:22 +0700

    information filling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+//form validation
+
 function emailIsValid (email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
-
 
 function validateForm() {
   var login = document.querySelector(".registration-form__input--login");
@@ -33,3 +34,31 @@ function validateForm() {
   window.alert("Registration successfully completed!");
   return true;
 }
+
+//Datepicker
+$(function() {
+  $( ".registration-form__input--date" ).datepicker({
+  dateFormat : 'dd/mm/yy',
+  changeMonth: true,
+  changeYear: true,
+  yearRange: "-100:+0"
+  }); 
+ });
+
+ //Card filling with the information
+ email.oninput = function() {
+  emailUser.innerHTML = email.value;
+};
+
+surname.oninput = function() {
+  surnameUser.innerHTML = surname.value;
+};
+
+user.oninput = function() {
+  userName.innerHTML = user.value;
+};
+
+login.oninput = function() {
+  loginUser.innerHTML = login.value;
+};
+
